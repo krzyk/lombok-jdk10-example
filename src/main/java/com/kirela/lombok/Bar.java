10d91c71f85b76343c704ec9070ecd3bbea0f68b
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2019-03-23 09:42:13 +0100

    update to test surefire with java 12

--- src/main/java/com/kirela/lombok/Bar.java
+++ src/main/java/com/kirela/lombok/Bar.java
@@ -1,7 +1,7 @@
 package com.kirela.lombok;
 
 public class Bar {
-    public static void main(String[] args) {
-        new Foo().setBar("abc");
+    public int foo() {
+        return 5;
     }
 }
--- src/main/java/com/kirela/lombok/Foo.java
+++
@@ -1,8 +0,0 @@
-package com.kirela.lombok;
-
-import lombok.Data;
-
-@Data
-public class Foo {
-    private String bar;
-}
